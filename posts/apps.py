32ded758661be4bb7486708c0ea2df03df858183
Alex Galygo <alex.galygo.94@gmail.com>
2024-11-17 19:00:09 +0300

    add models and admin

--- posts/apps.py
+++ posts/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class PostsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'posts'
+
+    # class Meta:
+    #     verbose_name = 'Пост'
+    #     verbose_name_plural = 'Посты'
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text[:50]
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
